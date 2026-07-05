26f639f97518cce44a63eef682f3cae645940be9
Lucas Galante <lsgalante12@gmail.com>
2026-07-04 20:18:53 -0400

    Implement set_rect for ProgressBar to account for label offset

--- src/widget/display/progress_bar.rs
+++ src/widget/display/progress_bar.rs
@@ -22,6 +22,14 @@ impl ProgressBar {
 
 impl Element for ProgressBar {
     crate::impl_widget_base!(ProgressBar);
+
+    fn set_rect(&mut self, x: f32, y: f32, w: f32, h: f32) {
+        self.base.x = x;
+        self.base.y = y;
+        self.base.w = w;
+        self.base.h = h + self.base.label_offset();
+    }
+
     fn highlight_quad(&self, _ctx: &UiContext) -> Option<(f32, f32, f32, f32, [f32; 4])>{ None }
 
     fn color(&self) -> [f32; 4] { colors::PROGRESS_BG }
